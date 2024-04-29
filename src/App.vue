cc4ce255e7eda433b958095baf8139cb0ecde912
qalqa <andreybas04@gmail.com>
2024-04-29 23:49:43 +0300

    feat: added props

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
-<script setup></script>
+<script setup>
+import TestComponent from './components/TestComponent.vue'
+</script>
 
-<template></template>
+<template>
+  <h1>My App works!</h1>
+  <TestComponent title="Кросовки Puma" price="9500" />
+  <TestComponent title="Кросовки Nike" price="11233" />
+  <TestComponent title="Кросовки Adiddas" price="12334" />
+  <TestComponent title="Кросовки Nike AirMax" price="67890" />
+  <TestComponent title="Кросовки Jordan" price="345678" />
+</template>
 
 <style scoped></style>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  title: String,
+  price: Number
+})
+</script>
+
+<template>
+  <div class="product">
+    <h2>Product: {{ title }}</h2>
+    <p>Price: {{ price }}</p>
+    <p>Added: {{ isAdded }}</p>
+  </div>
+</template>
+
+<style scoped>
+.product {
+  border: 1px solid blue;
+}
+</style>
