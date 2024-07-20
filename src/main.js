cd18452c8bb98dc4c7bd19d814404290fee4b235
qalqa <andreybas04@gmail.com>
2024-07-20 20:05:25 +0300

    feat: added Auth

--- src/main.js
+++ src/main.js
@@ -8,11 +8,15 @@ import App from './App.vue'
 import Home from './pages/Home.vue'
 import Favorites from './pages/Favorites.vue'
 import Profile from './pages/Profile.vue'
+import Auth from './pages/Auth.vue'
+import Register from './pages/Register.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/favorites', name: 'Favorites', component: Favorites },
-  { path: '/profile', name: 'Profile', component: Profile }
+  { path: '/profile', name: 'Profile', component: Profile },
+  { path: '/register', name: 'Register', component: Register },
+  { path: '/auth', name: 'Auth', component: Auth }
 ]
 
 const router = createRouter({
---
+++ src/pages/Auth.vue
@@ -0,0 +1,86 @@
+<script setup>
+import { useRouter } from 'vue-router'
+import DrawerHead from '@/components/DrawerHead.vue'
+import axios from 'axios'
+import { reactive } from 'vue'
+
+const router = useRouter()
+
+const goBack = () => {
+  router.push('/')
+}
+
+const user = reactive({
+  email: '',
+  password: ''
+})
+
+const login = async () => {
+  try {
+    const response = await axios.post('https://localhost:7228/login', {
+      email: user.email,
+      password: user.password
+    })
+    console.log(response.data)
+  } catch (error) {
+    console.error('Error:', error)
+  }
+}
+
+</script>
+
+<template>
+  <DrawerHead :back-to-func="goBack" :title="'Авторизация'" />
+  <div class="flex mt-7 test">
+    <div class="w-full">
+      <form @submit.prevent="login">
+        <ul class="w-full flex flex-col gap-5 justify-center">
+          <!-- <li class="relative">
+            <label for="login">Логин</label>
+            <input
+              id="login"
+              class="p-2 bg-color-soft inner-shadow px-4 transition outline-none rounded-md w-full mt-2"
+              type="text"
+              placeholder="Введите логин"
+            />
+          </li> -->
+          <li class="relative">
+            <label for="email">Email</label>
+            <input
+              id="email"
+              class="p-2 bg-color-soft inner-shadow px-4 transition outline-none rounded-md w-full mt-2"
+              type="email"
+              placeholder="email@example.com"
+              v-model="user.email"
+            />
+          </li>
+          <li class="relative">
+            <label for="password">Пароль</label>
+            <input
+              id="password"
+              class="p-2 bg-color-soft inner-shadow px-4 transition outline-none rounded-md w-full mt-2"
+              type="password"
+              placeholder="Введите пароль"
+              v-model="user.password"
+            />
+          </li>
+        </ul>
+        <button
+          type="submit"
+          class="mt-4 w-full font-bold rounded-xl p-3 hover-accent-shadow-box active:text-white disabled:opacity-50 disabled:shadow-none disabled:cursor-not-allowed transition cursor-pointer"
+        >
+          Сохранить
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+label {
+  position: absolute;
+  top: -5px;
+  left: 10px;
+  padding: 0 5px;
+}
+</style>
