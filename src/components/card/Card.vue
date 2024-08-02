9faa44ee2fc14c53ffe650cd4fd763f29c28a4b7
qalqa <andreybas04@gmail.com>
2024-08-02 22:43:47 +0300

    feat: some fixes for ui

--- src/components/card/Card.vue
+++ src/components/card/Card.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { inject } from 'vue'
 import { useRouter } from 'vue-router'
 import { useStore } from 'vuex'
 
@@ -8,13 +9,13 @@ const props = defineProps({
   title: String,
   price: Number,
   isFavorite: Boolean,
-  isAdded: Boolean,
-  onClickFavorite: Function,
-  onClickAdd: Function
+  isAdded: Boolean
 })
 
 const router = useRouter()
 const store = useStore()
+
+const isAuth = inject('isAuth')
 
 const goToDetails = () => {
   router.push({ name: 'CardOpened', params: { id: props.id } })
@@ -28,8 +29,7 @@ const goToDetails = () => {
     class="relative card-hitbox bg-color-soft p-10 rounded-xl p-8 transition hover:-translate-y-1 inner-shadow"
   >
     <img
-      v-if="onClickFavorite"
-      @click="onClickFavorite"
+      v-if="isAuth"
       :src="isFavorite ? '/like-2.svg' : '/like-1.svg'"
       alt="likeButton"
       class="absolute cursor-pointer z-10 top-8 left-8"
@@ -41,14 +41,6 @@ const goToDetails = () => {
         <span>Цена:</span>
         <b class="c-accent">{{ price }} руб.</b>
       </div>
-
-      <img
-        v-if="onClickAdd"
-        class="cursor-pointer"
-        @click="onClickAdd"
-        :src="isAdded ? '/checked.svg' : '/plus.svg'"
-        alt="plus"
-      />
     </div>
   </div>
 </template>
