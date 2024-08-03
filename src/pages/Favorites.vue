3a029ac900c19ad582a1ffe671865e3b25ea5764
qalqa <andreybas04@gmail.com>
2024-08-03 05:55:16 +0300

    feat: added empty message for favorites

--- src/pages/Favorites.vue
+++ src/pages/Favorites.vue
@@ -4,11 +4,13 @@ import { inject, onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 import DrawerHead from '@/components/cart/DrawerHead.vue'
+import infoBlock from '@/components/ui/infoBlock.vue'
 import NotAuthPlate from '@/components/ui/NotAuthPlate.vue'
 import List from '../components/home/List.vue'
 
 const favorites = ref([])
 const router = useRouter()
+const isEmpty = ref(false)
 
 const isAuth = inject('isAuth')
 const token = ref(inject('token'))
@@ -23,9 +25,14 @@ onMounted(async () => {
           Authorization: `Bearer ${token.value}`
         }
       })
-
-      favorites.value = data.map((obj) => obj)
+      console.log(data)
+      if (data) {
+        isEmpty.value = false
+        favorites.value = data.map((obj) => obj)
+        return
+      }
     } catch (error) {
+      isEmpty.value = true
       console.error(error)
     }
   }
@@ -44,7 +51,16 @@ const goToAuth = () => {
 <template>
   <div v-if="isAuth">
     <DrawerHead :back-to-func="goBack" :title="'Избранное'" />
-    <List :items="favorites" is-favorites />
+    <div v-if="!isEmpty">
+      <List :items="favorites" is-favorites />
+    </div>
+    <div v-else>
+      <infoBlock
+        imgUrl="../../emoji-1.png"
+        title="Тут пока ничего нету"
+        descr="Добавьте хотя бы одну пару кроссовок"
+      />
+    </div>
   </div>
   <div v-else>
     <NotAuthPlate :go-to-auth="goToAuth" />
